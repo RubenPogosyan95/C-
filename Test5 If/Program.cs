8c4bdcb0d25939b5773d80ccf001475dbe525cc6
Ruben <ruben.pogosyan95@gmail.com>
2022-11-09 22:21:13 +0400

    Test 5 Hello Ruben

---
+++ Test5 If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write UserName ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ruben")
+{
+    Console.WriteLine("Wow it's You RUBEN!!!");
+}
+else
+{
+    Console.WriteLine("Hello " + username);
+}
